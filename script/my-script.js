bb98f353292af882fae5209be63dabbd286627e7
herepremg@gmail.com <herepremg@gmail.com>
2017-05-08 23:20:33 +0530

    first-update

---
+++ script/my-script.js
@@ -0,0 +1,60 @@
+//lazy loader
+$("img.lazy").lazyload({
+    effect : "fadeIn"
+});
+// Add slideDown animation to Bootstrap dropdown when expanding.
+ $('.dropdown').on('show.bs.dropdown', function() {
+   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+ });
+
+ // Add slideUp animation to Bootstrap dropdown when collapsing.
+ $('.dropdown').on('hide.bs.dropdown', function() {
+   $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+ });
+
+
+ //stop iframe video, when modal video cancel
+ $(document).ready(function() {
+   $('#intro-modal-video').on('hidden.bs.modal', function() {
+     var $this = $(this).find('iframe'),
+       tempSrc = $this.attr('src');
+     $this.attr('src', "");
+     $this.attr('src', tempSrc);
+   });
+
+ //stop html5 video, when modal video cancel
+   $('#html5Video').on('hidden.bs.modal', function() {
+     var html5Video = document.getElementById("htmlVideo");
+     if (html5Video != null) {
+       html5Video.pause();
+       html5Video.currentTime = 0;
+     }
+   });
+ });
+
+
+
+
+ /**
+ * Vertically center Bootstrap 3 modals so they aren't always stuck at the top
+ */
+$(function() {
+    function reposition() {
+        var modal = $(this),
+            dialog = modal.find('.modal-dialog');
+        modal.css('display', 'block');
+
+        // Dividing by two centers the modal exactly, but dividing by three
+        // or four works better for larger screens.
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+    }
+    // Reposition when a modal is shown
+    $('.modal').on('show.bs.modal', reposition);
+    // Reposition when the window is resized
+    $(window).on('resize', function() {
+        $('.modal:visible').each(reposition);
+    });
+});
+
+
+//contact form
